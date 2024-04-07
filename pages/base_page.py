fdb26b4c3dfa0442d0c91fd750679311b7e56888
Evgenii <teror1716@yandex.ru>
2024-04-07 22:37:31 +0300

    Added tests to authorization page

--- pages/base_page.py
+++ pages/base_page.py
@@ -19,3 +19,16 @@ class BasePage:
     @allure.step("Find element by locator")
     def find(self, locator) -> Locator:
         return self.page.locator(locator)
+
+    @allure.step("Find a color of the element")
+    def find_color_of_element(self, locator) -> str:
+        element: Locator = self.find(locator)
+        color_value: str = element.evaluate("el => getComputedStyle(el).color")
+        return color_value
+
+    @allure.step("Check a color of the message")
+    def check_message_color(self, locator, expected_color):
+        current_color: str = self.find_color_of_element(locator)
+        assert (
+            expected_color == current_color
+        ), f"Current color is different = {current_color}"
