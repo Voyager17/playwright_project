d8334d6688495ad2d8c370ff5cab7d828c317c0e
Evgenii <teror1716@yandex.ru>
2024-04-13 18:38:13 +0300

    Fixed problem with tests

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,6 +3,7 @@ from pages.base_page import BasePage
 from pages.locators.locators import MainPageLocators as MainLoc
 from pages.locators.locators import RegistrationPageLocators as RegisterLoc
 from pages.locators.locators import TextColors as ColorLoc
+from pages.locators.locators import Texts as TextLoc
 from playwright.sync_api import expect, Locator
 
 
@@ -10,11 +11,27 @@ class MainPage(BasePage):
     page_url = "/"
 
     def login(self, login=LOGIN, password=PASSWORD) -> None:
+        registration_button: Locator = self.find(MainLoc.REGISTRATION_BUTTON)
+        login_field: Locator = self.find(RegisterLoc.LOGIN_BUTTON)
+        password_field: Locator = self.find(RegisterLoc.PASSWORD_BUTTON)
+        submit_button: Locator = self.find(RegisterLoc.SIGN_BUTTON)
+
         self.open()
-        self.find(MainLoc.REGISTRATION_BUTTON).click()
-        self.find(RegisterLoc.LOGIN_BUTTON).fill(login)
-        self.find(RegisterLoc.PASSWORD_BUTTON).fill(password)
-        self.find(RegisterLoc.SIGN_BUTTON).click()
+        self.page.wait_for_load_state("domcontentloaded")
+
+        registration_button.wait_for(state="visible")
+        registration_button.click()
+        self.page.wait_for_load_state("domcontentloaded")
+
+        login_field.wait_for(state="visible")
+        login_field.fill(login)
+
+        password_field.wait_for(state="visible")
+        password_field.fill(password)
+
+        submit_button.wait_for(state="visible")
+        submit_button.click()
+        self.page.wait_for_load_state("domcontentloaded")
 
     def check_authorization(self) -> None:
         self.page.hover(MainLoc.USERS_ICON_BUTTON)
@@ -69,24 +86,23 @@ class MainPage(BasePage):
         element: Locator = self.find(locator)
         expect(
             element, f"Locator has another text {element.text_content()}"
-        ).to_contain_text("Данные для входа неверны")
+        ).to_contain_text(TextLoc.WRONG_CREDENTIALS_ERROR)
 
     def check_error_authorization_text_empty(self, locator) -> None:
         element: Locator = self.find(locator)
         expect(
             element, f"Locator has another text {element.text_content()}"
-        ).to_contain_text("Поле должно быть заполнено")
+        ).to_contain_text(TextLoc.EMPTY_LOGIN_ERROR)
 
     def check_car_before_buying_incorrect_cases(self, number) -> None:
-        data_field: Locator = self.find("[data-ftid='autostory-widget_input']")
+        data_field: Locator = self.find(MainLoc.CAR_NUMBER_CHECKING_FIELD)
+        data_field.wait_for(state="visible")
         data_field.fill(number)
 
-        check_car_button: Locator = self.find(
-            "[data-ftid='autostory-widget_submit-button']"
-        )
+        check_car_button: Locator = self.find(MainLoc.CHECK_CAR_NUMBER_BUTTON)
         check_car_button.click()
-        error_message: Locator = self.find("[data-ftid='error_message']")
-        expect(error_message).to_contain_text(
-            "Введите корректный VIN / № кузова / госномер"
+        error_message: Locator = self.find(MainLoc.CAR_NUMBER_ERROR_TEXT)
+        expect(error_message).to_contain_text(TextLoc.CAR_NUMBER_ERROR)
+        self.check_message_color(
+            MainLoc.CAR_NUMBER_ERROR_TEXT, ColorLoc.CAR_NUMBER_ERROR_COLOR
         )
-        self.check_message_color("[data-ftid='error_message']", "rgb(219, 0, 26)")
