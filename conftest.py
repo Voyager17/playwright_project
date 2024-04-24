33438bd37655e741dd184c804d79898283d47a98
Evgenii <teror1716@yandex.ru>
2024-04-25 00:56:00 +0300

    Update conftest

--- conftest.py
+++ conftest.py
@@ -1,11 +1,37 @@
 import pytest
+from playwright.sync_api import (
+    sync_playwright,
+    Playwright,
+    Browser,
+    BrowserContext,
+    Page,
+)
+
 from pages.main_page import MainPage
-from playwright.sync_api import BrowserContext, Page
+
+
+@pytest.fixture(scope="session")
+def playwright() -> Playwright:
+    with sync_playwright() as p:
+        yield p
+
+
+@pytest.fixture(scope="session")
+def browser(playwright: Playwright) -> Browser:
+    browser = playwright.chromium.launch(headless=True)
+    yield browser
+    browser.close()
+
+
+@pytest.fixture(scope="function")
+def context(browser: Browser) -> BrowserContext:
+    context = browser.new_context()
+    yield context
+    context.close()
 
 
 @pytest.fixture()
-def page(playwright, context: BrowserContext) -> Page:
-    playwright.selectors.set_test_id_attribute("id")
+def page(context: BrowserContext) -> Page:
     page: Page = context.new_page()
     page.set_default_timeout(120000)
     page.set_viewport_size({"width": 1920, "height": 1080})
